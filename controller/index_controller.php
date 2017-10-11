ab1f4926fb60834f33f9efd02229d55fbcc0b096
bloodtear <xiaoyu1120520@126.com>
2017-10-11 18:26:36 +0800

    添加登录校验

--- controller/index_controller.php
+++ controller/index_controller.php
@@ -2,6 +2,9 @@
 include_once(dirname(__FILE__) . "/../config.php");
 
 class index_controller {
+    public function preaction($action) {
+        login::assert();
+    }
     public function index_action() {
         go("minichat/index/index");
     }
