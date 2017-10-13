cbf9a65d9929b0c7f92c9f42adff88b1cf440f02
bloodtear <xiaoyu1120520@126.com>
2017-10-13 17:24:45 +0800

    添加登录校验

--- controller/minichat/contact_controller.php
+++ controller/minichat/contact_controller.php
@@ -3,6 +3,9 @@ include_once(dirname(__FILE__) . "/../../config.php");
 include_once(dirname(__FILE__) . "/../../app/login.class.php");
 
 class contact_controller {
+    public function preaction($action) {
+        login::assert();
+    }
     public function index_action() {
         $tpl = new tpl("minichat/noheader", "minichat/chatfooter");
         $userid = get_session('user.id');
--- controller/minichat/me_controller.php
+++ controller/minichat/me_controller.php
@@ -3,6 +3,11 @@ include_once(dirname(__FILE__) . "/../../config.php");
 include_once(dirname(__FILE__) . "/../../app/login.class.php");
 
 class me_controller {
+    
+    public function preaction($action) {
+        login::assert();
+    }
+    
     public function index_action() {
         $tpl = new tpl("minichat/noheader", "minichat/chatfooter");
         $tpl->display("minichat/me/index");
